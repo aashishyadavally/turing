12032322372ea70c59c761011d4dc2580a552973
aashishyadavally <aashish.yadavally1995@gmail.com>
2019-05-13 02:59:14 -0400

    Add K-Nearest Neighbour algorithm

--- turing/models/clustering.py
+++ turing/models/clustering.py
@@ -8,6 +8,8 @@ Aashish Yadavally
 
 import sys
 import random
+import operator
+from collections import Counter
 import numpy as np
 
 
@@ -53,6 +55,96 @@ def find_centroid(items):
                 for i in range(num_axes)]
     return centroid
 
+
+class KNearestNeighbours:
+    """
+    Performs K-Nearest Neighbour Algorithm based classification on
+    the given dataset
+    """
+    def __init__(self, k, points, labels, max_iter=1000,
+                 distance_type='euclidean'):
+        """
+        Initializes KNearestNeighbours class
+
+        Args:
+            k (int):
+                Number of nearest neighbours to consider
+        """
+        self.points = points
+        length = {len(point) for point in self.points}
+        if len(length) != 1:
+            print("Input Error: All dataset points don't have same number\
+                  of attributes.")
+            sys.exit()
+        self.k = k
+
+        if len(points) != len(labels):
+            print("Number of dataset points is not the same as number of\
+                  labels.")
+            sys.exit()
+
+        self.distance_type = distance_type
+        self.dataset = dict(zip(self.points, labels))
+
+
+        for iteration in range(max_iter):
+            previous_labels = labels
+
+            for point in self.points:
+                self.dataset[point] = self.reassign_label(point)
+
+            self.labels = self.dataset.values()
+            if self.check_stopping_criterion(previous_labels):
+                print("Stopping criterion met, model training is being\
+                      stopped.")
+                break
+
+
+    def reassign_label(self, x):
+        """
+        Assigns new labels based on nearest neighbours
+
+        Args:
+            x (list):
+                Point in dataset whose label is to be re-assigned
+        """
+        dictionary = {}
+        for point, label in self.dataset.items():
+            dist = distance(x, point, self.distance_type)
+            dictionary[dist] = label
+        sorted_dist = sorted(dictionary.items(), key=operator.itemgetter(0))
+        majority_vote = self.voting(sorted_dist.values[:self.k])
+        return majority_vote
+
+    def voting(self, k_labels):
+        """
+        Returns the label which appears maximum number of times
+
+        Args:
+            k_labels (list):
+                Top 'k' labels whose distance is closest to the point
+
+        Returns:
+            (int):
+                Majority label
+        """
+        counts = Counter(k_labels)
+        return counts.most_common([1])[0][1]
+
+    def check_stopping_criterion(self, previous_labels):
+        """
+        Check if the dataset points labels are changing or not. Can stop the
+        model training if the dataset labels do not change
+
+        Args:
+            previous_labels (list):
+                Dataset points labels before reassignment
+
+        Returns:
+            (bool):
+                True, if dataset point label don't change, else False
+        """
+        return bool(previous_labels == self.labels)
 
 class KMeans:
     """
